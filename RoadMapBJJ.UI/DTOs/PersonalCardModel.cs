38fdd2555ef35b00ce65494961649f01feb7e5eb
niski1996 <niski1996@interia.pl>
2024-09-15 17:24:45 +0200

    caruzela

--- RoadMapBJJ.UI/DTOs/PersonalCardModel.cs
+++ RoadMapBJJ.UI/DTOs/PersonalCardModel.cs
@@ -1,3 +1,6 @@
+using RoadMapBJJ.Contracts.Entities.Media;
+using RoadMapBJJ.Contracts.Entities.Techniques;
+
 namespace RoadMapBJJ.UI.DTOs;
 
 public record PersonalCardModel(string Name, string LastName, string City, string Club, int Techniques);
---
+++ RoadMapBJJ.UI/DTOs/TechniqueCardModel.cs
@@ -0,0 +1,3 @@
+namespace RoadMapBJJ.UI.DTOs;
+
+public record TechniqueCardModel(string Thumbnail, string Name, string Description, int AssociatedTechniques, int AvailableVideos);
